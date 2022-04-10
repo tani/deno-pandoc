48cd0c741fd27673fc43de3e697a9af1cea746a4
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-04-10 10:57:13 +0000

    Remove rollup-plugin-deno

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,20 @@
-import deno from 'rollup-plugin-deno'
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
+import replace from '@rollup/plugin-replace'
 export default {
   input: './src/mod.js',
   output: {
     format: 'esm',
     file: './out/mod.js'
   },
-  plugins: [commonjs(), nodeResolve({preferBuiltins: false}), json(), deno()]
+  plugins: [
+    commonjs(),
+    nodeResolve(),
+    json(), 
+    replace({ 
+      punycode: 'https://deno.land/std/node/punycode.ts',
+      preventAssignment: true 
+    })
+  ]
 }
